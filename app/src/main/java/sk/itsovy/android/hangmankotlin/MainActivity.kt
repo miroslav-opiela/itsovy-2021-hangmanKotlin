9a08e7aa9cde3c28e29a19e7faba25b084ab0c2a
miroslav-opiela <miroslav-opiela@upjs.sk>
2021-10-01 11:21:19 +0200

    lateinit premenne na widgety

--- app/src/main/java/sk/itsovy/android/hangmankotlin/MainActivity.kt
+++ app/src/main/java/sk/itsovy/android/hangmankotlin/MainActivity.kt
@@ -3,11 +3,23 @@ package sk.itsovy.android.hangmankotlin
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
+import android.widget.EditText
+import android.widget.ImageView
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var imageView: ImageView
+    lateinit var textView: TextView
+    lateinit var editText: EditText
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        imageView = findViewById(R.id.imageViewGallows)
+        textView = findViewById(R.id.textViewGuessedWord)
+        editText = findViewById(R.id.editTextLetter)
     }
 
     fun onImageClick(view: View) {
